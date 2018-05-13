d4639a1fcd5e1b3e23eb8b8d1120b0f532a8d985
snlo <snloveydus@sina.com>
2018-05-13 10:52:55 +0800

    测试

--- SNNetworking/ViewController.m
+++ SNNetworking/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+//	pod trunk push SNNetworking.podspec --verbose --allow-warnings --use-libraries
 }
 
 
